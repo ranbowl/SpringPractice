e3e9d864b6dc0a11cb5f817a23b3f25a64b2db35
ranbowl <634239009@qq.com>
2019-06-08 00:58:55 -0500

    added log4j2

---
+++ src/main/java/com/springpractice/api/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.springpractice.api.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	private static final Logger logger = LogManager.getLogger(Controller.class);
+
+	@GetMapping("/hello")
+	public ResponseEntity<String> getHello() {
+		logger.info("Logger test here");
+		return ResponseEntity.ok("Hello world!");
+	}
+}
